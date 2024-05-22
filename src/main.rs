b6ad36d892976f86481a4a4f77eb85f1efe27a23
xiang <1335289286@qq.com>
2024-05-22 15:21:48 +0800

    ✨ feat: impl -L
    use -L to show all network interface

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 use clap::Parser;
+use colored::*;
+use pcap::Device;
 use rsp0f::Cli;
 
 fn main() {
+    println!("{}", format!("entry main {}", "#".repeat(25)).blue());
     let args = Cli::parse();
+    println!("{}", format!("all args: {:#?}", args).yellow());
+    if args.laani_flag {
+        println!("{}", "list all available network interface: ".yellow());
+        // 展示所有可用设备接口
+        for device in Device::list().unwrap() {
+            println!("Device: {:?}", device);
+        }
+    }
 
-    println!("{:?}", args);
+    println!("{}", format!("exit program {}", "#".repeat(25)).blue());
 }
